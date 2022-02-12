b9d717ed56d1f3bc0617036f86f5272678ddedac
ushieru <uzielcocolan@gmail.com>
2022-02-12 01:09:02 -0600

    first commit

---
+++ src/main/java/com/ibm/academia/restapi/tarjetas/TarjetasApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.academia.restapi.tarjetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/academia/restapi/tarjetas/TarjetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.academia.restapi.tarjetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarjetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
