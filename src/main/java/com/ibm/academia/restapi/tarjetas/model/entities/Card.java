774431aced5a46a6ea8a4b1145d03ad4a99887ed
ushieru <uzielcocolan@gmail.com>
2022-02-20 20:11:00 -0600

    Add validations

--- src/main/java/com/ibm/academia/restapi/tarjetas/model/entities/Card.java
+++ src/main/java/com/ibm/academia/restapi/tarjetas/model/entities/Card.java
@@ -8,7 +8,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +20,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "cards", schema = "private")
 public class Card {
@@ -24,21 +29,33 @@ public class Card {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
+    @NotNull
     @Column(name = "name")
     private String name;
 
+    @NotEmpty
+    @NotNull
     @Column(name = "passion")
     private String passion;
 
+    @Positive
+    @NotNull
     @Column(name = "max_salary")
     private BigDecimal maxSalary;
 
+    @Positive
+    @NotNull
     @Column(name = "min_salary")
     private BigDecimal minSalary;
 
+    @Positive
+    @NotNull
     @Column(name = "min_age")
     private Integer minAge;
 
+    @Positive
+    @NotNull
     @Column(name = "max_age")
     private Integer maxAge;
 
